dbebd6048e5814d18647ba0da0dee077d354edaa
dhuynh1182 <d11huynh@torontomu.ca>
2023-11-18 01:48:22 -0500

    parameter still needs editing
    also val accuracy stuck @ 25

--- 850_pro2.py
+++ 850_pro2.py
@@ -2,8 +2,10 @@ import numpy
 import pandas as pd
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Flatten, Conv2D,MaxPooling2D,Dropout
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from keras import layers
+
+from tensorflow.keras.preprocessing.image import random_shear
+from tensorflow.keras.utils import image_dataset_from_directory
 
 
 img_height, img_width, channel = 100,100,'rgb'
@@ -12,85 +14,84 @@ train_path = './Project 2 Data/Data/Train'
 val_path = './Project 2 Data/Data/Validation'
 test_path ='./Project 2 Data/Data/Test'
 
-# print("importing Data")
-# train_ds = image_dataset_from_directory(
-#     train_path,
-#     seed=501,
-#     image_size=(img_height, img_width),
-#     batch_size=32,
-#     color_mode = channel)
-
-# val_ds = image_dataset_from_directory(
-#     val_path,
-#     seed=501,
-#     image_size=(img_height, img_width),
-#     batch_size=32,
-#     color_mode = channel)
-
-# test_ds = image_dataset_from_directory(
-#     test_path,
-#     seed=501,
-#     image_size=(img_height, img_width),
-#     batch_size=32,
-#     color_mode = channel)
-
-#figure this out later
-train_gen = ImageDataGenerator(
-    shear_range = 0.2,
-    zoom_range = 0.2,
-    rescale = 1./255 )
-
-valid_gen = ImageDataGenerator(
-    rescale =1./255 
+print("importing Data")
+train_ds = image_dataset_from_directory(
+    train_path,
+    label_mode="categorical",
+    image_size=(100,100),
+    shuffle = True,
+    color_mode="rgb",
+    seed=501,
     )
 
-
-train_ds = train_gen.flow_from_directory(
-        train_path,  # this is the target directory
-        target_size=(100, 100),
-        color_mode = channel,
-        batch_size=32,
-        class_mode='binary')
-
-val_ds = valid_gen.flow_from_directory(
+val_ds = image_dataset_from_directory(
     val_path,
-    target_size= (100,100),
-    color_mode = channel,
-    batch_size = 32,
-    class_mode = 'binary'
+    label_mode="categorical",
+    image_size=(100,100),
+    color_mode="rgb",
+    shuffle = True,
+    seed=501,
     )
+
+val_ds = val_ds.map(lambda x, y: (layers.Rescaling(1.0 / 255)(x), y))
 
 #layers stuff
-# model = Sequential()
-# model.layer
-# model.add(Conv2D(32, (3, 3), activation='relu', input_shape=shape))
-# model.add(MaxPooling2D((2, 2)))
-# model.add(Flatten())
-# model.add(Dense(4))
+model = Sequential([
+    layers.Rescaling(1.0/255),
+    layers.RandomZoom(0.1),
+    layers.RandomFlip("horizontal"),
+    layers.RandomRotation(0.1),
+    
+    layers.Conv2D(16, 6, activation= 'relu'),
+    layers.MaxPooling2D(),
 
-model = Sequential()
-model.add(Conv2D(16, (11,11), input_shape=shape,activation = 'relu'))
-model.add(MaxPooling2D())
+    layers.Conv2D(32, 6, activation= 'relu'),
+    layers.MaxPooling2D(),
+    
+    layers.Conv2D(64, 3, activation= 'relu'),
+    layers.Conv2D(64, 3, activation= 'relu'),
+    layers.Conv2D(64, 3, activation= 'relu'),
+    layers.MaxPooling2D(),
+    
+    
+    layers.Flatten(),
+    layers.Dropout(0.5),
+    layers.Dense(4, activation='softmax'),
+])
 
-model.add(Conv2D(32, (5, 5), input_shape=shape,activation = 'relu'))
-model.add(MaxPooling2D())
-
-model.add(Conv2D(64, (3, 3), input_shape=shape,activation = 'relu'))
-model.add(MaxPooling2D())
 
 
-model.add(Flatten())
-model.add(Dropout(0.5))
-model.add(Dense(4))
-
-model.compile(optimizer='adam',
-              loss='binary_crossentropy',
+model.compile(optimizer='sgd',
+              loss='categorical_crossentropy',
               metrics=['accuracy'])
 
-model.fit(
+m = model.fit(
     train_ds,
     validation_data= val_ds,
     epochs = 10)
 
+
+acc = m.history['accuracy']
+val_acc = m.history['val_accuracy']
+loss = m.history['loss']
+val_loss = m.history['val_loss']
+
+import matplotlib.pyplot as plt
+
+epochs = range(1, len(acc) + 1)
+
+plt.plot(epochs, acc, label='Training acc')
+plt.plot(epochs, val_acc, 'r', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.legend()
+
+plt.figure()
+
+plt.plot(epochs, loss, label='Training loss')
+plt.plot(epochs, val_loss, 'r', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+
+plt.show()
 
 
